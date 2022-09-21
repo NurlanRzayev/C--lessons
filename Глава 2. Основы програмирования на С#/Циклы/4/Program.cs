2d4d1390b77e63e75ce20a213abb988408ccc8ae
Nurlan Rzayev <nurlanrza03@gmail.com>
2022-09-21 22:30:01 +0400

    third commit

---
+++ Глава 2. Основы програмирования на С#/Циклы/4/Program.cs
@@ -0,0 +1,15 @@
+﻿while(true)
+{
+    Console.WriteLine("Введите первое число:");
+    int num1 = Convert.ToInt32(Console.ReadLine());
+    Console.WriteLine("Введите второе число:");
+    int num2 = Convert.ToInt32(Console.ReadLine());
+
+    if(num1 < 0 || num1 > 10 || num2 < 0 || num2 > 10)
+        Console.WriteLine("Оба числа должны быть в диапозоне от 0 до 10");
+    else
+    {
+        Console.WriteLine($"{num1} * {num2} = {num1 * num2}");
+        break;
+    }
+}
